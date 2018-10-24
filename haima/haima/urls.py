1874bcbb239ddad14cebee8eb46b59e495f54943
nmsl <992388632@qq.com>
2018-10-24 21:56:43 +0800

    我的评价

--- haima/haima/urls.py
+++ haima/haima/urls.py
@@ -43,12 +43,7 @@ urlpatterns = [
     path('release_auction/', view.release_auction),
     path('test/', view.text_message),
     path('test_ajax', view.test_ajax),
-<<<<<<< HEAD
-=======
-
     path('test_qiniu/', view.test_qiniu),
-    path('callback/', view.callback),
->>>>>>> 27a3060ef40763ff668b63de801234f27b06cabd
     path('my_auction/', view.my_auction),
     path('my_collection/', view.my_collection),
     path('history_auction/', view.history_auction),
@@ -57,13 +52,9 @@ urlpatterns = [
     path('my_evaluate/', view.my_evaluate),
     path('modify_information/', view.modify_information),
     path('leave_message/', view.leave_message),
-<<<<<<< HEAD
     path('publish_auction/', view.publish_auction),
     path('release_auction_ok/', view.release_auction_ok),
-=======
     path('publish_auction/',view.publish_auction),
     path('release_auction_ok/',view.release_auction_ok),
     path('buy_auction/',view.buy_auction),
->>>>>>> 27a3060ef40763ff668b63de801234f27b06cabd
-
 ]
