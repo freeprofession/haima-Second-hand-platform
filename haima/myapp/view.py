316408d7ec2cfe52fd89d16d7ab44a4fed0c62ff
623668415 <623668415@qq.com>
2018-11-15 08:32:15 +0800

    修改端口号

--- haima/myapp/view.py
+++ haima/myapp/view.py
@@ -580,10 +580,7 @@ def goods_list(request):
                         bvalue_list = list(cut_words.smembers(key))
                         for value in bvalue_list:
                             value = int(value.decode('utf-8'))
-<<<<<<< HEAD
-=======
 
->>>>>>> 504549eb71b65954aab5b95587fd51ac61e7b8d4
                             if value not in value_list:
                                 value_list.append(value)
                                 cur.execute(
@@ -595,10 +592,7 @@ def goods_list(request):
                                 else:
                                     cut_words.srem(key, value)
                     prompt = '已选条件： 所有与' + '"' + question + '"' + '相关的宝贝'
-<<<<<<< HEAD
-=======
 
->>>>>>> 504549eb71b65954aab5b95587fd51ac61e7b8d4
         if category == '1':
             cur.execute(
                 "select goods_id,goods_title,goods_imgurl,goods_price from t_goods where goods_category_id=%s and goods_state = %s",
@@ -2595,9 +2589,9 @@ def get_ali_object():
 
     # 支付完成后，支付偷偷向这里地址发送一个post请求，识别公网IP,如果是 192.168.20.13局域网IP ,支付宝找不到，def page2() 接收不到这个请求
     # notify_url = "http://47.94.172.250:8804/page2/"
-    notify_url = "http://127.0.0.1:8000/page2/"
+    notify_url = "http://127.0.0.1:80/page2/"
     # 支付完成后，跳转的地址。
-    return_url = "http://127.0.0.1:8000/page2/"
+    return_url = "http://127.0.0.1:80/page2/"
     merchant_private_key_path = "keys/app_private_2048.txt"  # 应用私钥
     alipay_public_key_path = "keys/alipay_public_2048.txt"  # 支付宝公钥
     alipay = AliPay(
