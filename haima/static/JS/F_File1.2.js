426b72eaf05c077eaec2895679c7934d0862173c
pfl <473512059@qq.com>
2018-11-19 19:47:58 +0800

    form

--- haima/static/JS/F_File1.2.js
+++ haima/static/JS/F_File1.2.js
@@ -164,7 +164,7 @@ FFile.prototype = {
         // console.log(dex);
         // console.log(fileArray)
         console.log(postArray)
-        // form.submit();
+        form.submit();
 
         // $.ajax({
         //     type: "POST",
